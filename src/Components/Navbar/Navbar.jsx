0048be75097a911e7519e61aab6525c8a241d63e
HamzaaAftab <163124480+HamzaaAftab@users.noreply.github.com>
2024-05-31 07:04:27 -0700

    ""

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,53 +1,63 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
-
-
-
 const Navbar = () => {
-  
   const [sticky, setSticky] = useState(false);
-  useEffect(()=>{
-    window.addEventListener('scroll', ()=>{
-      window.scrollY > 500 ? setSticky(true) : setSticky(false)
-    })
-  }, [])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 500 ? setSticky(true) : setSticky(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <nav
-      className={`w-[100%] px-6 py-3 fixed top-0 left-0 flex items-center justify-between z-10 container1 text-white ${sticky? 'dark-nav' : ` `}`}
+      className={`w-[100%] px-6 py-3 fixed top-0 left-0 flex items-center justify-between z-10 container1 text-white ${
+        sticky ? "dark-nav" : ``
+      }`}
     >
-      <img src={logo} alt="" className={`w-[200px]`} />
-      <ul
-        className={`flex list-none my-[5px] mx-[2px] text-[17px] gap-9 font-medium items-center `}
-      >
+      <img src={logo} alt="Logo" className="w-[200px]" />
+      <ul className="flex list-none my-[5px] mx-[2px] text-[17px] gap-9 font-medium items-center">
         <li>
-        <a href="#">Home</a>
+          <Link to="heroo" smooth={true} duration={500}>
+            Home
+          </Link>
         </li>
         <li>
-          <a href="#">Program</a>
+          <Link to="program" smooth={true} duration={500} offset={-260}>
+            Program
+          </Link>
         </li>
         <li>
-          <a href="#">About Us </a>
+          <Link to="about" smooth={true} duration={500} offset={-150}>
+            About Us
+          </Link>
         </li>
         <li>
-          <a href="#">Campus</a>
+          <Link to="campus" smooth={true} duration={500} offset={-260}>
+            Campus
+          </Link>
         </li>
         <li>
-          <a href="#">Testimonials</a>
+          <Link to="testimonials" smooth={true} duration={500}>
+            Testimonials
+          </Link>
         </li>
         <li>
-          <a
-            href="#_"
-            class="relative inline-flex items-center justify-start  px-5 py-3 overflow-hidden font-medium transition-all  rounded-full hover:bg-white group"
+          <Link to="contact" smooth={true} duration={500} offset={-260} className="relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-medium transition-all rounded-full hover:bg-white group"
           >
-            <span class="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-white rounded-full"></span>
-            <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-blue-600">
+            <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-white rounded-full"></span>
+            <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-blue-600">
               Contact us
             </span>
-          </a>
+            </Link>
         </li>
       </ul>
     </nav>
